697e5f1f2cb493f4bc28c5df2c87a5713a02ad01
Jony Devnath <76802139+jonydevnath@users.noreply.github.com>
2025-01-17 00:03:19 +0600

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,13 +1,19 @@
 <?php
+session_start(); // Start the session
+
 // Check if the user is logged in
 if (!isset($_SESSION['user_id'])) {
     header("Location: signin.php"); // Redirect to the sign-in page if not logged in
     exit();
 }
 
-session_start();
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
-header("Location: signin.php"); // Redirect to sign-in page
+// Unset all session variables
+session_unset();
+
+// Destroy the session
+session_destroy();
+
+// Redirect to the sign-in page
+header("Location: signin.php");
 exit();
 ?>
