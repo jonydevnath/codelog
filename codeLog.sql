b5ce77f13dd42753a59f6056f4cebebdcda137b3
jonydevnath <76802139+jonydevnath@users.noreply.github.com>
2025-02-26 01:09:03 +0600

    updated the codelog sql

--- codeLog.sql
+++ codeLog.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: sql102.infinityfree.com
--- Generation Time: Jan 18, 2025 at 10:52 AM
--- Server version: 10.6.19-MariaDB
--- PHP Version: 7.2.22
+-- Host: localhost
+-- Generation Time: Feb 25, 2025 at 08:06 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `if0_37834939_codelog`
+-- Database: `codeLog`
 --
 
 -- --------------------------------------------------------
@@ -33,14 +32,14 @@ CREATE TABLE `admin_users` (
   `email` varchar(100) NOT NULL,
   `pwd` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admin_users`
 --
 
 INSERT INTO `admin_users` (`admin_id`, `email`, `pwd`, `created_at`) VALUES
-(1, 'jonydevnath@gmail.com', 'jonydevnath@gmail.com', '2024-12-22 15:25:03');
+(1, 'admin@gmail.com', 'admin@gmail.com', '2024-12-19 17:12:54');
 
 -- --------------------------------------------------------
 
@@ -53,16 +52,37 @@ CREATE TABLE `comments` (
   `post_id` int(10) UNSIGNED NOT NULL,
   `user_name` varchar(100) NOT NULL,
   `commnt` text NOT NULL,
-  `created_at` datetime DEFAULT current_timestamp()
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `comments`
 --
 
 INSERT INTO `comments` (`comment_id`, `post_id`, `user_name`, `commnt`, `created_at`) VALUES
-(1, 1, 'jonydevnath', 'fgf', '2025-01-16 09:27:39'),
-(2, 1, 'jonydevnath', 'Good!', '2025-01-16 10:06:14');
+(1, 16, 'jhon', 'it was a great post..', '2024-12-20 22:40:58'),
+(6, 16, 'jhon', 'Thanks a lots!', '2024-12-22 19:13:55'),
+(7, 16, 'admin', 'Thanks!', '2024-12-22 19:15:10'),
+(8, 16, 'jhonn', 'nice!', '2025-02-22 10:08:46');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `like_id` int(10) UNSIGNED NOT NULL,
+  `post_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `likes`
+--
+
+INSERT INTO `likes` (`like_id`, `post_id`, `user_id`) VALUES
+(1, 16, 8);
 
 -- --------------------------------------------------------
 
@@ -73,19 +93,19 @@ INSERT INTO `comments` (`comment_id`, `post_id`, `user_name`, `commnt`, `created
 CREATE TABLE `posts` (
   `post_id` int(10) UNSIGNED NOT NULL,
   `title` varchar(255) NOT NULL,
+  `category` varchar(50) NOT NULL,
   `img` varchar(255) DEFAULT NULL,
   `post` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `posts`
 --
 
-INSERT INTO `posts` (`post_id`, `title`, `img`, `post`, `created_at`, `updated_at`) VALUES
-(1, 'I Stopped Using Kubernetes. Our DevOps Team Is Happier Than Ever', 'uploads/Kubernetes.webp', 'Six months ago, our DevOps team was drowning in complexity. We were managing 47 Kubernetes clusters across three cloud providers.', '2024-12-22 15:28:20', '2024-12-22 15:37:12'),
-(2, 'I Stopped Using Kubernetes. Our DevOps Team Is Happier Than Ever', 'uploads/1734881794_Kubernetes.webp', 'Six months ago, our DevOps team was drowning in complexity. We were managing 47 Kubernetes clusters across three cloud providers.', '2024-12-22 15:36:34', '2024-12-22 15:36:34');
+INSERT INTO `posts` (`post_id`, `title`, `category`, `img`, `post`, `created_at`, `updated_at`) VALUES
+(16, 'I Stopped Using Kubernetes. Our DevOps Team Is Happier Than Ever', 'Programming', 'uploads/amzon.webp', 'Six months ago, our DevOps team was drowning in complexity. We were managing 47 Kubernetes clusters across three cloud providers. hi', '2024-12-19 08:28:44', '2025-02-22 12:25:28');
 
 -- --------------------------------------------------------
 
@@ -105,14 +125,14 @@ CREATE TABLE `users` (
   `web_url` varchar(200) DEFAULT NULL,
   `birthday` date DEFAULT NULL,
   `gender` enum('Male','Female','Other') DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`user_id`, `username`, `user_email`, `user_pass`, `profile_image`, `full_name`, `phone`, `country`, `web_url`, `birthday`, `gender`) VALUES
-(1, 'jonydevnath', 'jonydevnath@gmail.com', '$2y$10$X5uXdZVg3gxWOMfx7HyPS.hpUsKzgMQFVvU3oBbhZl7Niw6/WVVee', 'admin/uploads/6789405110f5a_ben_10.png', '', '', 'Bangladesh', '', '0000-00-00', 'Male');
+(8, 'jonydevnath', 'jonydevnath@gmail.com', '$2y$10$hIqzBOwaRVlcXM44Nh6uaOgnWHaY.M0ofMI9rJ3Nvbo7CW9XyL6fy', 'assets/user.png', 'Jony Devnath', '019054142363', 'Bangladesh', 'https://jonydevnath.github.io/', '2000-01-26', 'Male');
 
 --
 -- Indexes for dumped tables
@@ -129,7 +149,15 @@ ALTER TABLE `admin_users`
 --
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`comment_id`),
-  ADD KEY `fk_post` (`post_id`);
+  ADD KEY `fk_posts1` (`post_id`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`like_id`),
+  ADD UNIQUE KEY `post_id` (`post_id`,`user_id`),
+  ADD KEY `fk_users` (`user_id`);
 
 --
 -- Indexes for table `posts`
@@ -151,25 +179,48 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admin_users`
 --
 ALTER TABLE `admin_users`
-  MODIFY `admin_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `admin_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comment_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `comment_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `like_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `post_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `post_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `fk_posts1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `fk_posts2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
